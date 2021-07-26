e0d1fd5d9eb34c1bef84c41412e5be3ea3b9b267
Mauricio Aspiazo <aspiazo@google.com>
2021-07-26 15:54:47 -0500

    Modelos para trazos y recaudos

--- src/creditoo/cartera/admin.py
+++ src/creditoo/cartera/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Clientes, Persona
+from .models import Cliente, Persona
 from .models import ObligacionFinanciera
 
 # Register your models here.
 admin.site.register(Persona)
 admin.site.register(ObligacionFinanciera)
-admin.site.register(Clientes)
+admin.site.register(Cliente)
--- src/creditoo/cartera/models.py
+++ src/creditoo/cartera/models.py
@@ -7,6 +7,7 @@ from django.db.models.fields.related import ForeignKey
 # Create your models here.
 
 class Persona(models.Model):
+    readonly_fields = ('creado', 'actualizado')
     TIPO_IDENTIFICACION = (
         ('CC', 'Cedula'),
         ('CE', 'Cedula Extranjeria'),
@@ -15,15 +16,20 @@ class Persona(models.Model):
 
     numero_identificacion = models.BigIntegerField()
     tipo_identification = models.CharField(max_length=2, choices=TIPO_IDENTIFICACION, default='CC')
-    fecha_expedicion = models.DateField(null=True)
-    nombre_completo = models.CharField(max_length=150)
+    fecha_expedicion = models.DateField(null=True, blank=True)
+    primer_nombre = models.CharField(max_length=100)
+    segundo_nombre = models.CharField(max_length=100)
+    primer_apellido = models.CharField(max_length=100)
+    segundo_apellido = models.CharField(max_length=100)
     telefono = models.CharField(max_length=100)
     direccion = models.CharField(max_length=100)
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.nombre_completo
 
-class Clientes(models.Model):
+class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     url_logo = models.CharField(max_length=150)
 
@@ -36,18 +42,45 @@ class Clientes(models.Model):
 
 
 class ObligacionFinanciera(models.Model):
-    persona_id = models.ForeignKey(Persona, on_delete=models.CASCADE, verbose_name='Deudor')
-    cliente_id = models.ForeignKey(Clientes, on_delete=models.CASCADE, verbose_name='Cliente')
+    readonly_fields = ('creado', 'actualizado')
+    
+    persona = models.ForeignKey(Persona, on_delete=models.CASCADE, verbose_name='Deudor')
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name='Cliente')
     numero_referencia = models.CharField(max_length=100)
-    estado_obligacion = models.CharField(max_length=150)
-    fecha_ultimo_pago = models.DateField(null=True)
-    dias_mora = models.IntegerField()
-    saldo_vencido = models.FloatField()
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
 
     class Meta:
         verbose_name = 'Obligacion Financiera'
         verbose_name_plural = 'Obligaciones Financieras'
 
-    
+class ActualizacionObligacion(models.Model):
+    readonly_fields = ('creado', 'actualizado')
 
+    obligacion = models.ForeignKey(ObligacionFinanciera, verbose_name='Obligacion Financiera')
+    periodo = models.CharField(max_length=100)
+    estado_obligacion = models.CharField(max_length=150)
+    fecha_ultimo_pago = models.DateField(null=True)
+    tramo_inicial = models.CharField(max_length=150)
+    dias_mora = models.IntegerField()
+    saldo_vencido = models.FloatField(verbose_name="Saldo en Mora")
+    total_pagar = models.FloatField(verbose_name="Total a Pagar")
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Obligacion'
+        verbose_name_plural = 'Tramos de Obligacion'
+
+class AcuerdoPago(models.Model):
+    readonly_fields = ('creado', 'actualizado')
+
+    obligacion = models.ForeignKey(ObligacionFinanciera, verbose_name='Obligacion Financiera')
+    cuotas = models.IntegerField()
+    valor_cuota = models.FloatField()
+    creado = models.DateTimeField(auto_now_add=True)
+    actualizado = models.DateTimeField(auto_now=True)
+
+class Recaudo(models.Model):
+    obligacion = models.ForeignKey(ObligacionFinanciera, verbose_name='Obligacion Financiera')
 
