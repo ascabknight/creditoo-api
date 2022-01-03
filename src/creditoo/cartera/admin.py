c71939a97fd45d1adaa5ae808abe046776ea25a6
Hector Cohen <hectorcohenmaldonado@gmail.com>
2022-01-03 09:25:14 -0500

    changes

--- src/creditoo/cartera/admin.py
+++ src/creditoo/cartera/admin.py
@@ -2,13 +2,8 @@ from django.contrib import admin
 from .models import Cliente, Persona, ObligacionFinanciera, CuentasPorCobrar, AcuerdoPago
 
 # Register your models here.
-admin.site.register(Persona)
-admin.site.register(ObligacionFinanciera)
-admin.site.register(Cliente)
-admin.site.register(AcuerdoPago)
-
 class CuentasPorCobrarAdmin(admin.ModelAdmin):
-    list_display = ['get_obligacion_cliente', 'get_obligacion_persona', 'saldo_vencido']
+    list_display = ['get_obligacion_cliente', 'get_obligacion_persona', 'saldo_vencido', 'obligacion']
     
     @admin.display(description='Cliente')
     def get_obligacion_cliente(self, obj):
@@ -19,4 +14,20 @@ class CuentasPorCobrarAdmin(admin.ModelAdmin):
         return obj.obligacion.persona
 
 
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'primer_nombre', 'primer_apellido', 'numero_identificacion', 'email']
+
+class AcuerdoPagoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'obligacion', 'valor_cuota', 'fecha_compromiso']
+
+class ObligacionFinancieraAdmin(admin.ModelAdmin):
+    list_display = ['cliente', 'numero_referencia']
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'url_logo']
+
 admin.site.register(CuentasPorCobrar, CuentasPorCobrarAdmin)
+admin.site.register(Persona, PersonaAdmin)
+admin.site.register(AcuerdoPago, AcuerdoPagoAdmin)
+admin.site.register(ObligacionFinanciera, ObligacionFinancieraAdmin)
+admin.site.register(Cliente, ClienteAdmin)
