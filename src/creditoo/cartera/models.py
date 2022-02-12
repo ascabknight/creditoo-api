36489c0c78db993fe8469772adcfd221464ffa19
Hector Cohen <hectorcohenmaldonado@gmail.com>
2022-02-12 10:19:48 -0500

    last

--- src/creditoo/cartera/models.py
+++ src/creditoo/cartera/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.db.models.base import Model
 
+
 class Persona(models.Model):
     readonly_fields = ('creado', 'actualizado')
     list_display = ("numero_identificacion", "nombre_completo")
-    
+
     numero_identificacion = models.IntegerField(unique=True)
     tipo_identification = models.CharField(max_length=2, default='CC')
     nombre_completo = models.CharField(max_length=100)
@@ -17,6 +18,7 @@ class Persona(models.Model):
     def __str__(self):
         return f"{self.nombre_completo}"
 
+
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     url_logo = models.CharField(max_length=150)
@@ -28,9 +30,12 @@ class Cliente(models.Model):
         verbose_name = 'Cliente'
         verbose_name_plural = 'Clientes'
 
+
 class ObligacionFinanciera(models.Model):
-    persona = models.ForeignKey(Persona, on_delete=models.CASCADE, verbose_name='Deudor')
-    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE, verbose_name='Cliente')
+    persona = models.ForeignKey(
+        Persona, on_delete=models.CASCADE, verbose_name='Deudor')
+    cliente = models.ForeignKey(
+        Cliente, on_delete=models.CASCADE, verbose_name='Cliente')
     numero_referencia = models.CharField(max_length=100)
 
     def __str__(self):
@@ -40,20 +45,23 @@ class ObligacionFinanciera(models.Model):
         verbose_name = 'Obligacion Financiera'
         verbose_name_plural = 'Obligaciones Financieras'
 
+
 class CuentasPorCobrar(models.Model):
     readonly_fields = ('creado', 'actualizado')
 
-    obligacion = models.ForeignKey(ObligacionFinanciera,on_delete=models.CASCADE, verbose_name='Obligacion')
+    obligacion = models.ForeignKey(
+        ObligacionFinanciera, on_delete=models.CASCADE, verbose_name='Obligacion')
     periodo = models.CharField(max_length=100)
     estado_obligacion = models.CharField(max_length=150)
     fecha_ultimo_pago = models.DateTimeField(blank=True, null=True)
+    fecha_limite_pago = models.DateTimeField()
     tramo = models.CharField(max_length=150)
-    dias_mora = models.IntegerField()
+    dias_mora = models.IntegerField(blank=True, null=True)
     saldo_vencido = models.FloatField(verbose_name="Saldo en Mora")
     total_pagar = models.FloatField(verbose_name="Total a Pagar")
     creado = models.DateTimeField(auto_now_add=True)
     actualizado = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         verbose_name = 'Cuenta por cobrar'
         verbose_name_plural = 'Cuentas por Cobrar'
@@ -62,14 +70,16 @@ class CuentasPorCobrar(models.Model):
 class AcuerdoPago(models.Model):
     readonly_fields = ('creado', 'actualizado')
 
-    obligacion = models.ForeignKey(ObligacionFinanciera, on_delete=models.CASCADE, verbose_name='Obligacion')
+    obligacion = models.ForeignKey(
+        ObligacionFinanciera, on_delete=models.CASCADE, verbose_name='Obligacion')
     cuotas = models.IntegerField()
     fecha_compromiso = models.DateField()
     valor_cuota = models.FloatField()
-    estado=models.BooleanField(default=False)
+    estado = models.BooleanField(default=False)
     comentarios = models.CharField(max_length=200)
     creado = models.DateTimeField(auto_now_add=True)
     actualizado = models.DateTimeField(auto_now=True)
+
 
 class EstudioCredito(models.Model):
     readonly_fields = ('creado', 'actualizado')
@@ -86,9 +96,11 @@ class EstudioCredito(models.Model):
     monto_solicitado = models.FloatField(default=0)
     cuotas = models.IntegerField(default=0)
     documento_adjunto = models.CharField(max_length=200)
-    estado = models.CharField(max_length=20, choices=ESTADOS, default='EN_ESTUDIO')
+    estado = models.CharField(
+        max_length=20, choices=ESTADOS, default='EN_ESTUDIO')
     creado = models.DateTimeField(auto_now_add=True)
     actualizado = models.DateTimeField(auto_now=True)
+
     class Meta:
         verbose_name = 'Estudio De Credito'
         verbose_name_plural = 'Estudios de Credito'
